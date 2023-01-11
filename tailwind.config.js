99e47d0e365c560671d31f31202f5f8b32ae5f89
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-11 11:47:59 +0100

    Changement CSS et config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./Vues/**/*.php'],
+    content: ['./Vue/**/*.php', './css/**/*.css'],
     theme: {
         extend: {},
     },
     daisyui: {
         themes: [{
                 mytheme: {
-                    primary: "#a991f7",
-                    secondary: "#f6d860",
-                    accent: "#37cdbe",
-                    neutral: "#3d4451",
-                    "base-100": "#ffffff",
+                    primary: "#BCB8AA",
+                    secondary: "#BCB8AA",
+                    accent: "#BCB8AA",
+                    neutral: "#BCB8AA",
+                    "base-100": "#D6C8BE",
                 },
             },
             "dark",
-            "cupcake",
+            "cupcake", "light",
         ],
     },
     plugins: [require("daisyui")],
