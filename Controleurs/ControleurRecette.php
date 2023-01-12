34b615a529e37e2ac9d34ff84765909606bcf5f0
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-12 23:42:42 +0100

    Affiche la page résultat de la searchBar

--- Controleurs/ControleurRecette.php
+++ Controleurs/ControleurRecette.php
@@ -7,5 +7,15 @@ final class ControleurRecette
       $recette = $model->getRecette($params[0]);
       Vue::montrer('recette/pageRecette', array('recette' => $recette));
     }
+
+  public function searchAction()
+  {
+    require_once("Modele/helpers.php");
+    $db = new PDO("mysql:host=localhost;dbname=test", "root", "root");
+    $model = new Recette($db);
+    $recettes = $model->searchRecette($_GET['search']);
+    Vue::montrer('recette/pageSearchResult', array('recettes' => $recettes));
+  }
+      
 }
 ?>
