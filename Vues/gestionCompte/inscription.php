076069fe5a8025fb3da6f189d8aae487212a42d2
eliseeLeydier <elisee.leydier@etu.univ-amu.fr>
2023-01-16 16:30:25 +0100

    input inscrire plus jolie

--- Vues/gestionCompte/inscription.php
+++ Vues/gestionCompte/inscription.php
@@ -2,12 +2,9 @@
 <p class="text-base-200 text-warning"><?= $A_vue['compte'][0]['Pseudo'] ?></p>
 <p> s'inscrire </p>
 <form action="/Compte/inscrire/" method=get>
-  <label for="email">Pseudo :</label>
-  <input type="text" id="email" name="pseudo" style="border: 1px solid black;"><br><br>
-  <label for="email">Email :</label>
-  <input type="email" id="email" name="email" style="border: 1px solid black;"><br><br>
-  <label for="password">Mot de passe :</label>
-  <input type="password" id="password" name="password" style="border: 1px solid black;"><br><br>
+  <input type="text" id="email" placeholder="pseudo" name="pseudo"><br><br>
+  <input type="email" id="email" name="email" placeholder="Email"><br><br>
+  <input type="password" id="password" name="password" placeholder="Password"><br><br>
   <input type="submit" value="Envoyer" style="border: 1px solid black;">
 </form>
 <a href="/compte/connexion" class="btn btn-accent bg-secondary">J'ai déja un compte</a>
