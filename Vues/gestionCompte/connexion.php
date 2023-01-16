add6cdae24316daa107925850819796126da4ef3
eliseeLeydier <elisee.leydier@etu.univ-amu.fr>
2023-01-16 16:31:45 +0100

    Input Connecter plus jolie

--- Vues/gestionCompte/connexion.php
+++ Vues/gestionCompte/connexion.php
@@ -1,9 +1,7 @@
 <p> se connecter </p>
 <form action="/Compte/connecter/" method=get>
-  <label for="email">Email :</label>
-  <input type="email" id="email" name="email" style="border: 1px solid black;"><br><br>
-  <label for="password">Mot de passe :</label>
-  <input type="password" id="password" name="password" style="border: 1px solid black;"><br><br>
+  <input type="email" id="email" name="email" placeholder="Email"><br><br>
+  <input type="password" id="password" name="password" placeholder="Mot de passe"><br><br>
   <input type="submit" value="Envoyer" style="border: 1px solid black;">
 </form>
 <a href="/compte/inscription" class="btn btn-accent bg-secondary">Je n'ai pas de compte</a>
