c935c0eb54ce4cdc031e849ede8f69a505cf567e
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-16 09:46:08 +0100

    Page erreur si recherche recette vide

--- Vues/recette/pageSearchResult.php
+++ Vues/recette/pageSearchResult.php
@@ -1,5 +1,13 @@
 <div class="flexSearchRecettes" >
 <?php
+if(!isset($A_vue['recettes'][0]['titre'])) {?>
+    <div class="alert alert-error shadow-lg">
+  <div>
+    <span>Aucune recette n'a été trouvé !</span>
+  </div>
+</div>
+<?php
+}
 $i = 0;
 while(isset($A_vue['recettes'][$i]['titre'])) { 
 ?>
