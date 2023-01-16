5052ad489cf6f6dddb0be06901a487613e8f6ac2
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-16 23:12:09 +0100

    Ajout récupération de la table catégorie

--- Controleurs/ControleurDefaut.php
+++ Controleurs/ControleurDefaut.php
@@ -7,9 +7,11 @@ final class ControleurDefaut
     {   
         require_once("Modele/helpers.php");
         $db = new PDO("mysql:host=mysql-sweet-spot.alwaysdata.net;dbname=sweet-spot_db", "296154","sweetspot123");
-        $model = new Recette($db);
-        $recette = $model->get3Recettes();
-        Vue::montrer('accueil/introduction', array('recette' => $recette));
+        $recetteModel = new Recette($db);
+        $categorieModel = new Categorie($db);
+        $recette = $recetteModel->get3Recettes();
+        $categorie = $categorieModel->getCategories();
+        Vue::montrer('accueil/introduction', array('recette' => $recette, 'categorie' => $categorie));
         
     }
 
