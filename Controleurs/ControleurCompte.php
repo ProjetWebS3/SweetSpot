f3c18e9e18c70be4c86e8cb3d8924121f8c5bdf7
eliseeLeydier <elisee.leydier@etu.univ-amu.fr>
2023-01-18 09:38:16 +0100

    Reglage bug de retrun de parametre indésirable

--- Controleurs/ControleurCompte.php
+++ Controleurs/ControleurCompte.php
@@ -9,26 +9,26 @@ final class ControleurCompte
     }
 
     public function connexionAction(){
-        Vue::montrer('gestionCompte/connexion', array('compte' => $compte));
+        Vue::montrer('gestionCompte/connexion');
     }
 
     public function inscriptionAction(){
-        Vue::montrer('gestionCompte/inscription', array('compte' => $compte));
+        Vue::montrer('gestionCompte/inscription');
     }
     
     public function inscrireAction()
     {
     $db = new PDO("mysql:host=mysql-sweet-spot.alwaysdata.net;dbname=sweet-spot_db", "296154","sweetspot123");
     $model = new Compte($db);
-    $compte = $model->submitAction($_GET['pseudo'], $_GET['email'], $_GET['password'], 0);
-    Vue::montrer('gestionCompte/Inscription', array('Inscription' => $compte));
+    $model->submitAction($_GET['pseudo'], $_GET['email'], $_GET['password'], 0);
+    Vue::montrer('gestionCompte/Inscription');
     }
 
     public function connecterAction(){
         $db = new PDO("mysql:host=mysql-sweet-spot.alwaysdata.net;dbname=sweet-spot_db", "296154","sweetspot123");
         $model = new Compte($db);
-        $compte = $model->getCompteAction($_GET['email'], $_GET['password']);
-        Vue::montrer('gestionCompte/Connexion', array('Connexion' => $compte));        
+        $model->getCompteAction($_GET['email'], $_GET['password']);
+        Vue::montrer('gestionCompte/Connexion');        
         if ( $_SESSION['login'] == true) {
             //var_dump("Bonjour, ", $_SESSION['login']['Pseudo']);
         } else {
