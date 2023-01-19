285ff158ce8f575879a1160ca2abe406b9a18614
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-19 01:05:59 +0100

    rendre responsive pageResult+accueil

--- Vues/accueil/introduction.php
+++ Vues/accueil/introduction.php
@@ -6,7 +6,7 @@ for ($i = 0; $i < 3; $i++) {
         <figure><img src="data:image/png;base64,<?= base64_encode($A_vue['recette'][$i]['image']) ?>" alt="<?= $A_vue['recette'][$i]['titre'] ?>"></figure>
         <div class="card-body">
             <h2 class="card-title text-base-200"><?= $A_vue['recette'][$i]['titre'] ?></h2>
-            <p class="text-base-200 text-warning">Note: <?= rating_stars($A_vue['recette'][$i]['note']) ?></p>
+            <p class="text-base-200 text-warning">Note:<br> <?= rating_stars($A_vue['recette'][$i]['note']) ?></p>
             <div class="card-actions justify-end">
             <a href="/recette/show/<?= $A_vue['recette'][$i]['id_recette'] ?>" class="btn btn-accent bg-secondary">Recette</a>
         </div>
