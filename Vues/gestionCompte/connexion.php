fa64fcdcdc8a7c0f2a1b9cd558780456c0e9567b
EthanTESTA <118427837+EthanTESTA@users.noreply.github.com>
2023-01-20 11:03:51 +0100

    ajout html/css de la connexion

--- Vues/gestionCompte/connexion.php
+++ Vues/gestionCompte/connexion.php
@@ -1,12 +1,24 @@
-
 <p class="text-base-200 text-warning">
   Bonjour
-<?= $_SESSION['login']['Pseudo'] ?> <br>
+<?= $_SESSION['pseudo'] ?> <br>
 </p>  
-<a href="/compte/inscription" class="btn btn-ghost">Je n'ai pas de compte</a>
 
-<form action="/Compte/connecter/" method=get>
-  <input type="email" id="email" name="email" placeholder="Email" class="input w-full max-w-xs"><br><br>
-  <input type="password" id="password" name="password" placeholder="Mot de passe" class="input w-full max-w-xs"><br><br>
-  <input type="submit" value="Envoyer" style="border: 1px solid black;" class="btn btn-outline">
-</form>
+<div class="bg-white" data-theme="mytheme">
+  <div class="ori mx-auto my-10 w-full max-w-xs">
+    <form class="mb-4 rounded-3xl bg-base-100 px-8 pt-6 pb-8 shadow-md" action="/Compte/connecter/">
+      <div class="mb-4">
+        <label class="mb-2 block text-sm font-bold text-gray-700" for="email"> Email </label>
+        <input name="email" class="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none" id="email" type="text" placeholder="Email" />
+      </div>
+      <div class="mb-6">
+        <label class="mb-2 block text-sm font-bold text-gray-700" for="password">Mot de passe</label>
+        <input name="password" class="focus:shadow-outline mb-3 w-full appearance-none rounded border border-red-600 py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none" id="password" type="password" placeholder="**********" />
+        <p class="text-xs italic text-red-600">Veuillez entrer un mot de passe.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <input type="submit" value="Envoyer" style="border: 1px solid black;" class="focus:shadow-outline rounded bg-accent py-2 px-4 font-bold text-white hover:bg-accent-focus focus:outline-none" />
+        <a class="inline-block align-baseline text-sm font-bold text-accent hover:text-accent-focus" href="/compte/inscription"> S'inscrire </a>
+      </div>
+    </form>
+  </div>
+</div>
