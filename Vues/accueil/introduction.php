b5da2d4b1fbd04053d7c4af75705c35e20539eb4
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-23 15:07:05 +0100

    changement de l'affichage des catégories

--- Vues/accueil/introduction.php
+++ Vues/accueil/introduction.php
@@ -16,72 +16,41 @@ for ($i = 0; $i < 3; $i++) {
 }
 ?>
 </div>
-<div data-theme ="mytheme" class = "bg-white">
+<div data-theme ="mytheme" class="bg-white m-5 mb-20">
   <h2 class="text-2xl font-bold tracking-tight text-gray-900">Liste des catégories</h2>
   </br>
   <h2 class="text-2x2 font-bold tracking-tight text-gray-900">Difficultés</h2>
 
   <div class="grid grid-cols-4 gap-4 h-16 font-medium">
-    <button class="rounded-md bg-base-100 " type="button">
-      Très Facile
-    </button>
-    <button class="rounded-md bg-base-100 " type="button">
-      Facile
-    </button>
-    <button class="rounded-md bg-base-100 " type="button">
-      Moyen
-    </button>
-    <button class="rounded-md bg-base-100  " type="button">
-      Difficile
-    </button>
+    <?php 
+    for ($i = 0; $i < 4; $i++) { ?>
+      <button  onclick="location.href='/Recette/searchByCategory/<?=$A_vue['categorie'][$i]['id_type']?>';" class="rounded-md bg-base-100" type="button">
+        <?=$A_vue['categorie'][$i]['nom'] ?>
+      </button>
+    <?php } ?>
   </div>
   </br>
 
   <h2 class="text-2x2 font-bold tracking-tight text-gray-900">Coût</h2>
   <div class="grid grid-cols-4 gap-4 h-16 font-medium">
-    <button class="rounded-md bg-secondary " type="button">
-      Peu cher
-    </button>
-    <button class="rounded-md bg-secondary basis-1/4" type="button">
-      Abordable
-    </button>
-    <button class="rounded-md bg-secondary basis-1/4" type="button">
-      Cher
-    </button>
-    <button class="rounded-md bg-secondary basis-1/4" type="button">
-      Très cher
-    </button>
+    <?php 
+    for ($i = 8; $i < 12; $i++) { ?>
+      <button  onclick="location.href='/Recette/searchByCategory/<?=$A_vue['categorie'][$i]['id_type']?>';" class="rounded-md bg-secondary basis-1/4" type="button">
+        <?=$A_vue['categorie'][$i]['nom'] ?>
+      </button>
+    <?php } ?>
   </div>
   </br>
 
   <h2 class="text-2x2 font-bold tracking-tight text-gray-900">Durée</h2>
-  <div class="grid grid-cols-4 gap-4 h-16 font-bold text-white">
-    <button class="rounded-md bg-accent" type="button">
-      Rapide
-    </button>
-    <button class="rounded-md bg-accent " type="button">
-      Normale
-    </button>
-    <button class="rounded-md bg-accent" type="button">
-      Moyenne
-    </button>
-    <button class="rounded-md bg-accent" type="button">
-      Longue
-    </button>
-  </div>
+
+    <div class="grid grid-cols-4 gap-4 h-16 font-bold text-white">
+      <?php 
+      for ($i = 4; $i < 8; $i++) { ?>
+        <button  onclick="location.href='/Recette/searchByCategory/<?=$A_vue['categorie'][$i]['id_type']?>';" class="rounded-md bg-accent" type="button">
+          <?=$A_vue['categorie'][$i]['nom'] ?>
+        </button>
+      <?php } ?>
+    </div>
+
 </div> 
-
-<h2 class="text-xl my-5">Listes des catégories</h2>
-
-<div class="flex w-full flex-row flex-wrap justify-center">
-  <?php
-    $i = 0;
-  while(isset($A_vue['categorie'][$i]['nom'])) { 
-  ?>
-  <a href="/Recette/searchByCategory/<?=$A_vue['categorie'][$i]['id_type']?>"><div style="background-color: <?=getProperColor($i)?>" class="grid flex-grow  h-28 w-28 card card-normal bg-base-300 rounded-box place-items-center"><p class="font-semibold"><?=$A_vue['categorie'][$i]['type']?></p><?=$A_vue['categorie'][$i]['nom']?></div></a>
-  <div class="divider lg:divider-horizontal"></div> 
-  <?php 
-    $i++;
-  }
-  ?>
-</div>
