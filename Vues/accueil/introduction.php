799f8657609103d8e31fc487b211bf986506bffd
Lucas Leiner <98752773+LucasLeiner@users.noreply.github.com>
2023-01-25 00:15:59 +0100

    Ajout boutons admin

--- Vues/accueil/introduction.php
+++ Vues/accueil/introduction.php
@@ -2,16 +2,20 @@
 <?php
 for ($i = 0; $i < 3; $i++) {
 ?>
-    <div data-theme="mytheme" class="flexRecette card card-compact bg-accent shadow-xl" >
-        <figure><img src="data:image/png;base64,<?= base64_encode($A_vue['recette'][$i]['image']) ?>" alt="<?= $A_vue['recette'][$i]['titre'] ?>"></figure>
-        <div class="card-body">
-            <h2 class="card-title text-base-200"><?= $A_vue['recette'][$i]['titre'] ?></h2>
-            <p class="text-base-200 text-warning">Note:<br> <?= rating_stars($A_vue['recette'][$i]['note']) ?></p>
-            <div class="card-actions justify-end">
-            <a href="/recette/show/<?= $A_vue['recette'][$i]['id_recette'] ?>" class="btn btn-accent bg-secondary">Recette</a>
+  <div data-theme="mytheme" class="flexRecette card card-compact bg-accent shadow-xl" >
+    <figure><img src="data:image/png;base64,<?= base64_encode($A_vue['recette'][$i]['image']) ?>" alt="<?= $A_vue['recette'][$i]['titre'] ?>"></figure>
+    <div class="card-body">
+      <div class="flex">
+        <h2 class="card-title text-base-200"><?= $A_vue['recette'][$i]['titre'] ?></h2>
+        <h3 class="text-base-200 ml-3"><i class="fa-solid fa-trash fa-xl"></i><i class="fa-solid fa-pen-to-square fa-xl"></i></h3>
+      </div>
+      <p class="text-base-200 text-warning">Note:<br> <?= rating_stars($A_vue['recette'][$i]['note']) ?></p>
+        <div class="card-actions justify-end">
+          <a href="/recette/show/<?= $A_vue['recette'][$i]['id_recette'] ?>" class="btn btn-accent bg-secondary">Recette</a> 
         </div>
+      
     </div>
-</div>
+  </div>
 <?php
 }
 ?>
