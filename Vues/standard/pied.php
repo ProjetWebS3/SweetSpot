37b288f84674d7be1d7d6e43e4317766964778cd
Lucas Leiner <98752773+LucasLeiner@users.noreply.github.com>
2023-01-26 11:55:22 +0100

    Footer fait, sans mentions légales

--- Vues/standard/pied.php
+++ Vues/standard/pied.php
@@ -1,1 +1,3 @@
-<footer>Pied</footer>
+<footer class="bg-gray-800 text-gray-300 flex p-2 justify-center bottom-0 left-0 right-0 fixed mt-5">
+  <a href="">Mentions légales</a><p1 class="ml-3 mr-3">--</p1><p1>Tous droits réservés sweet-spot.alwaysdata.net 2023-2023</p1>
+</footer>
