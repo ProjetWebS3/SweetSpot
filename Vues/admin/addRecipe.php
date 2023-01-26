a897f86258eff5c9edb15e8af442f09d7613caf0
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-26 23:52:17 +0100

    Ajout required form

--- Vues/admin/addRecipe.php
+++ Vues/admin/addRecipe.php
@@ -5,7 +5,7 @@
   <form class="bg-pink-50 p-10 rounded-md" action="/admin/addRecipe" method="post" enctype="multipart/form-data">
     <div class="mb-5">
       <label for="name" class="mb-3 block text-base font-medium text-[#07074D]"> Nom de la recette </label>
-      <input type="text" name="titre" id="name" placeholder="Gateau au chocolat" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+      <input type="text" name="titre" id="name" placeholder="Gateau au chocolat" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" required/>
     </div> <br>
       <label for="name" class="mb-3 block text-base font-medium text-[#07074D]"> Choisir une  image
         <span class="sr-only">Choose a recipe picture</span>
@@ -15,7 +15,7 @@
         file:text-sm file:font-semibold
         file:bg-gray-50 file:text-gray-700
         hover:file:bg-gray-100
-        " id="fileInput" name="image" onchange="checkFile()"/>
+        " id="fileInput" name="image" onchange="checkFile()" required/>
         <p class="mt-1 text-xs text-gray-500" id="file_input_help">PNG, JPG (doit faire exactement 600x400).</p>
       </label><br>
     <div class="mb-5">
@@ -47,11 +47,11 @@
     </div><br><br>
     <div class="mb-5">
       <label for="message" class="mb-3 block text-base font-medium text-[#07074D]"> Ingrédients </label>
-      <textarea rows="4" name="ingredient" id="message" placeholder="- ... &#10 - ... &#10 - ..."class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"></textarea>
+      <textarea rows="4" name="ingredient" id="message" placeholder="- ... &#10 - ... &#10 - ..."class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" required></textarea>
     </div>
     <div class="mb-5">
       <label for="message" class="mb-3 block text-base font-medium text-[#07074D]"> Préparation </label>
-      <textarea rows="4" name="description" id="message" placeholder="1 ère Etape : &#10 ..." class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"></textarea>
+      <textarea rows="4" name="description" id="message" placeholder="1 ère Etape : &#10 ..." class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" required></textarea>
     </div>
     <button class="btn bg-gray-400 text-white justify-center"><input type="submit" name="soumission" value="Submit"><br></button> 
   </form>
