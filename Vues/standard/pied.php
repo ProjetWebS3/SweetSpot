ba2796a1bda4db01358c9cf22f3ca54f571d60d1
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-01-26 11:59:01 +0100

    Changement style footer

--- Vues/standard/pied.php
+++ Vues/standard/pied.php
@@ -1,3 +1,3 @@
-<footer class="bg-gray-800 text-gray-300 flex p-2 justify-center bottom-0 left-0 right-0 fixed mt-5">
+<footer data-theme="mytheme" class="flex p-2 justify-center bottom-0 left-0 right-0 fixed mt-8">
   <a href="">Mentions légales</a><p1 class="ml-3 mr-3">--</p1><p1>Tous droits réservés sweet-spot.alwaysdata.net 2023-2023</p1>
 </footer>
